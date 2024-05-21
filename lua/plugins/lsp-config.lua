d48a068849cc8162c5070f2f48c1a1451bde9363
seanlau <lthon0225@gmail.com>
2024-05-21 12:27:22 +0800

    update auto formatting for null ls

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -8,14 +8,14 @@ return {
       -- black
       -- isort
       -- spell
-    end
+    end,
   },
   {
     "williamboman/mason-lspconfig.nvim",
     lasy = false,
     opts = {
-      auto_install = true
-    }
+      auto_install = true,
+    },
     -- config = function()
     --   require("mason-lspconfig").setup({
     --     ensure_installed = { "lua_ls", "tsserver", "zls" }
@@ -26,26 +26,20 @@ return {
     "neovim/nvim-lspconfig",
     lasy = false,
     config = function()
-      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
       local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({
-        capabilities = capabilities
-      })
-      lspconfig.tsserver.setup({
-        capabilities = capabilities
-      })
-      lspconfig.zls.setup({
-        capabilities = capabilities
-      })
-      lspconfig.html.setup({
-        capabilities = capabilities
-      })
-      vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, {})
-      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-      vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, {})
-      vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {})
-      vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, {})
-    end
-  }
+      local servers = { "tsserver", "lua_ls", "zls", "html" }
+      for _, lsp in ipairs(servers) do
+        lspconfig[lsp].setup({
+          capabilities = capabilities,
+        })
+      end
+      vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, {})
+      vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
+      vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, {})
+    end,
+  },
 }
