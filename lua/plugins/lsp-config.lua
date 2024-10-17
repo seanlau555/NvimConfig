f0e1d5870de92955cdab81b4b6e8515b43e8f97b
seanlau <lthon0225@gmail.com>
2024-10-17 10:23:49 +0800

    update lsp

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -34,8 +34,8 @@ return {
           capabilities = capabilities,
         })
       end
-      vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
-      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { noremap = true })
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, { noremap = true })
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, {})
       vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
