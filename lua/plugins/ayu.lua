d77f7f2ad71ca3bf6e0a60bbb77bb8790be8d048
seanlau <lthon0225@gmail.com>
2025-01-07 12:36:29 +0800

    feat: add fzf finder

--- lua/plugins/ayu.lua
+++ lua/plugins/ayu.lua
@@ -1,26 +1,45 @@
 return {
-	"projekt0n/github-nvim-theme",
-	name = "github-theme",
-	lazy = false, -- make sure we load this during startup if it is your main colorscheme
-	priority = 1000, -- make sure to load this before all the other start plugins
-	config = function()
-		require("github-theme").setup({})
-		vim.cmd("colorscheme github_dark")
-	end,
 	-- {
-	--   "folke/tokyonight.nvim",
-	--   lazy = false,
-	--   priority = 1000,
-	--   opts = {},
+	--   "projekt0n/github-nvim-theme",
+	--   name = "github-theme",
+	--   lazy = false,  -- make sure we load this during startup if it is your main colorscheme
+	--   priority = 1000, -- make sure to load this before all the other start plugins
 	--   config = function()
-	--     vim.cmd.colorscheme("tokyonight")
+	--     require("github-theme").setup({})
+	--     vim.cmd("colorscheme github_dark")
 	--   end,
 	-- },
 	-- {
-	--   "morhetz/gruvbox",
+	-- 	"folke/tokyonight.nvim",
+	-- 	lazy = false,
+	-- 	priority = 1000,
+	-- 	opts = {},
+	-- 	config = function()
+	-- 		vim.cmd.colorscheme("tokyonight")
+	-- 	end,
+	-- },
+	-- {
+	--   "ribru17/bamboo.nvim",
+	--   lazy = false,
+	--   priority = 1000,
 	--   config = function()
-	--     vim.cmd.colorscheme("gruvbox")
+	--     require("bamboo").setup({
+	--       -- optional configuration here
+	--     })
+	--     require("bamboo").load()
 	--   end,
+	-- },
+	{
+		"rebelot/kanagawa.nvim",
+		config = function()
+			vim.cmd.colorscheme("kanagawa")
+		end,
+	},
+	-- {
+	-- 	"morhetz/gruvbox",
+	-- 	config = function()
+	-- 		vim.cmd.colorscheme("gruvbox")
+	-- 	end,
 	-- },
 	-- {
 	--   "challenger-deep-theme/vim",
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,27 +1,45 @@
 return {
-  {
-    'nvim-telescope/telescope.nvim', tag = '0.1.6',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    config = function()
-      local builtin = require("telescope.builtin")
-      vim.keymap.set('n', '<leader>g', builtin.find_files, {})
-      vim.keymap.set('n', '<leader>f', builtin.live_grep, {})
-    end
-  },
-  {
-    "nvim-telescope/telescope-ui-select.nvim",
-    config = function()
-      require("telescope").setup {
-        extensions = {
-          ["ui-select"] = {
-            require("telescope.themes").get_dropdown {
-            }
-          }
-        }
-      }
-      require("telescope").load_extension("ui-select")
-    end
-  }
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.6",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			local builtin = require("telescope.builtin")
+			-- vim.keymap.set("n", "<leader>g", builtin.git_files, {})
+			vim.keymap.set("n", "<leader>G", builtin.find_files, {})
+			vim.keymap.set("n", "<leader>f", builtin.live_grep, {})
+		end,
+	},
+	{
+		"nvim-telescope/telescope-fzf-native.nvim",
+		build = "make",
+		config = function()
+			require("telescope").setup({
+				--  defaults   = {},
+				--  pickers    = {},
+				extensions = {
+					fzf = {
+						fuzzy = true, -- false will only do exact matching
+						override_generic_sorter = true, -- override the generic sorter
+						override_file_sorter = true, -- override the file sorter
+						case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+					},
+				},
+			})
+			require("telescope").load_extension("fzf")
+		end,
+	},
+	{
+		"nvim-telescope/telescope-ui-select.nvim",
+		config = function()
+			require("telescope").setup({
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown({}),
+					},
+				},
+			})
+			require("telescope").load_extension("ui-select")
+		end,
+	},
 }
-
-
