f6c6b47f290f6fb567f777934f39235394521efc
seanlau <lthon0225@gmail.com>
2025-08-11 17:08:58 +0800

    update nvim tree

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,45 +1,44 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-			-- stylua
-			-- prettier
-			-- black
-			-- isort
-			-- spell
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		lasy = false,
-		opts = {
-			auto_install = true,
-		},
-		-- config = function()
-		--   require("mason-lspconfig").setup({
-		--     ensure_installed = { "lua_ls", "tsserver", "zls" }
-		--   })
-		-- end
-	},
-	{
-		"neovim/nvim-lspconfig",
-		lasy = false,
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-			local lspconfig = require("lspconfig")
-			local servers = { "ts_ls", "lua_ls", "html" }
-			for _, lsp in ipairs(servers) do
-				lspconfig[lsp].setup({
-					capabilities = capabilities,
-				})
-			end
-			vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { noremap = true })
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, { noremap = true })
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, {})
-			vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
-			vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, {})
-		end,
-	},
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+      -- stylua
+      -- prettier
+      -- black
+      -- isort
+      -- spell
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    lasy = false,
+    opts = {
+      auto_install = true,
+    },
+    -- config = function()
+    --   require("mason-lspconfig").setup({
+    --     ensure_installed = { "lua_ls", "tsserver", "zls" }
+    --   })
+    -- end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    lasy = false,
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      local lspconfig = require("lspconfig")
+      local servers = { "ts_ls", "lua_ls", "html" }
+      for _, lsp in ipairs(servers) do
+        lspconfig[lsp].setup({
+          capabilities = capabilities,
+        })
+      end
+      vim.keymap.set("n", "gD", vim.lsp.buf.definition, { noremap = true })
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, {})
+      vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
+      vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, {})
+    end,
+  },
 }
--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,46 +1,53 @@
 return {
-  -- {
-  --   "nvim-tree/nvim-tree.lua",
-  --   dependencies = {
-  --     "nvim-tree/nvim-web-devicons",
-  --   },
-  --   config = function()
-  --     require("nvim-tree").setup({
-  --     })
-  --     vim.keymap.set('n', '<C-v>', ':NvimTreeToggle<CR>', {})
-  --     vim.keymap.set('n', '<C-y>', ':NvimTreeFindFile<CR>', {})
-  --     vim.keymap.set('n', '<C-r>', ':NvimTreeRefresh<CR>', {})
-  --   end
-  -- }
   {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
+    "nvim-tree/nvim-tree.lua",
     dependencies = {
-      "nvim-lua/plenary.nvim",
       "nvim-tree/nvim-web-devicons",
-      "MunifTanjim/nui.nvim",
     },
     config = function()
-      require("neo-tree").setup({
-        git = {
-          enable = true,
-          timeout = 500, -- (in ms)
-        },
-        filesystem = {
-          filtered_items = {
-            visible = true,
-            show_hidden_count = true,
-            hide_dotfiles = false,
-            hide_gitignored = false,
+      require("nvim-tree").setup({
+        actions = {
+          open_file = {
+            window_picker = {
+              enable = false,
+            },
           },
         },
       })
-      vim.keymap.set("n", "<C-v>", ":Neotree toggle<CR>", {})
-      vim.keymap.set("n", "<C-y>", ":Neotree filesystem reveal left<CR>", {})
-      buffers = { follow_current_file = { enabled = true } }
-      -- vim.keymap.set('n', '<C-y>', ':Neotree focus filesystem left<CR>', {})
-      -- nnoremap <silent> <C-v> :NERDTreeToggle<CR>
-      -- nnoremap <silent> <C-y> :NERDTreeFind<CR>
+      vim.keymap.set("n", "<C-v>", ":NvimTreeToggle<CR>", {})
+      vim.keymap.set("n", "<C-y>", ":NvimTreeFindFile<CR>", {})
+      vim.keymap.set("n", "<C-r>", ":NvimTreeRefresh<CR>", {})
     end,
   },
+  -- {
+  --   "nvim-neo-tree/neo-tree.nvim",
+  --   branch = "v3.x",
+  --   dependencies = {
+  --     "nvim-lua/plenary.nvim",
+  --     "nvim-tree/nvim-web-devicons",
+  --     "MunifTanjim/nui.nvim",
+  --   },
+  --   config = function()
+  --     require("neo-tree").setup({
+  --       git = {
+  --         enable = true,
+  --         timeout = 500, -- (in ms)
+  --       },
+  --       filesystem = {
+  --         filtered_items = {
+  --           visible = true,
+  --           show_hidden_count = true,
+  --           hide_dotfiles = false,
+  --           hide_gitignored = false,
+  --         },
+  --       },
+  --     })
+  --     vim.keymap.set("n", "<C-v>", ":Neotree toggle<CR>", {})
+  --     vim.keymap.set("n", "<C-y>", ":Neotree filesystem reveal left<CR>", {})
+  --     buffers = { follow_current_file = { enabled = true } }
+  --     -- vim.keymap.set('n', '<C-y>', ':Neotree focus filesystem left<CR>', {})
+  --     -- nnoremap <silent> <C-v> :NERDTreeToggle<CR>
+  --     -- nnoremap <silent> <C-y> :NERDTreeFind<CR>
+  --   end,
+  -- },
 }
